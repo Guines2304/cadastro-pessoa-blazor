c7b91af29c5568a85a46a71c92e1bf5d889e6791
Guines2304 <guilhermeosilguerra@gmail.com>
2024-07-31 14:16:21 -0300

    Add files via upload

---
+++ CadastroPessoas/CadastroPessoas/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using CadastroPessoas.Models;
+using Microsoft.EntityFrameworkCore;
+    namespace CadastroPessoas.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+    DbSet<Pessoas> Pessoas { get; set; } = null!;
+}
---
+++ CadastroPessoas/CadastroPessoas/Models/Pessoas.cs
@@ -0,0 +1,9 @@
+﻿namespace CadastroPessoas.Models;
+
+public class Pessoas
+{
+    public string id { get; set; }
+    public string nome { get; set; } = string.Empty;
+    public string cpf { get; set; } = string.Empty;
+    public string email { get; set; } = string.Empty;
+}
